cdcb630df19d295f916370cde6d6b28a12d7ad63
Poonam123-qa <poonamg.qa.com>
2024-10-09 00:32:46 +0530

    Adding Numbers

---
+++ src/main/java/JavaPractice/AddNum.java
@@ -0,0 +1,16 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class AddNum {
+    public static void main(String[] args) {
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        num1 = sc.nextInt();
+        System.out.println("Enter second number");
+        num2 = sc.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum is " + sum);
+    }
+}
