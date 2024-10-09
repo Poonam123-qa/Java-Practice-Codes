d13cf109597ac88565f88a6662254adad70c0803
Poonam123-qa <poonamg.qa.com>
2024-10-09 21:26:28 +0530

    Swapping of the numbers

---
+++ src/main/java/JavaPractice/SwapNumb.java
@@ -0,0 +1,22 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class SwapNumb {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("The First numbers is :" + num1+" "+num2);
+        num1 = num1+num2;
+        num2 = num1-num2;
+        num1 = num1-num2;
+
+        System.out.println("The swappig of the numbers is:"+num1+" "+num2);
+
+    }
+}
