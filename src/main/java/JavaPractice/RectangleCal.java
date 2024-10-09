6997cd8bfadcefaaf0367122228a8c6e53c2a1b7
Poonam123-qa <poonamg.qa.com>
2024-10-09 21:16:46 +0530

    Calculate rectangle with scanner class

---
+++ src/main/java/JavaPractice/RectangleCal.java
@@ -0,0 +1,21 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class RectangleCal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle");
+        double length = sc.nextDouble();
+
+        System.out.println("Enter the width of the rectangle");
+        double width = sc.nextDouble();
+
+        double area = length * width;
+        double perimeter = 2 * length * width;
+
+        System.out.println("The area of the rectangle is " + area);
+        System.out.println("The perimeter of the rectangle is " + perimeter);
+    }
+}
+
