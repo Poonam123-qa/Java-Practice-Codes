6eb19e3f73a32de6ba8ea30512c4e8195743e002
Poonam123-qa <poonamg.qa.com>
2024-10-09 21:07:31 +0530

    Circle calculations with scanner class

---
+++ src/main/java/JavaPractice/CircleCal.java
@@ -0,0 +1,17 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class CircleCal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+        double perimeter = 2 * Math.PI * radius;
+
+        System.out.println("The area of the circle is " + area);
+        System.out.println("The perimeter of the circle is " + perimeter);
+    }
+}
