083081c6548ec741a5992fc0ab5e23b8498d8e0e
Poonam123-qa <poonamg.qa.com>
2024-10-09 20:35:56 +0530

    Divided Numbers

---
+++ src/main/java/JavaPractice/DivideNum.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class DivideNum {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        int a=sc.nextInt();
+
+        System.out.println("Enter the second number");
+        int b =sc.nextInt();
+
+        double c= (double) a /b;
+
+        System.out.println("The divided number is..."+c);
+    }
+}
