4284a4fe03c649997e2acae2e120376134b34ae6
Poonam123-qa <poonamg.qa.com>
2024-10-13 20:56:39 +0530

    Current date and time

---
+++ src/main/java/JavaPractice/CurrentDateandTime.java
@@ -0,0 +1,12 @@
+package JavaPractice;
+
+import java.util.Date;
+
+public class CurrentDateandTime {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+        System.out.println(date);
+        //System.out.println(date.getTime());
+    }
+}
