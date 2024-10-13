763b996be6446d089c346d9f37a546e58a211e89
Poonam123-qa <poonamg.qa.com>
2024-10-13 23:04:42 +0530

    Converting the string into lower case.

---
+++ src/main/java/JavaPractice/StringLower.java
@@ -0,0 +1,12 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class StringLower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        System.out.println("The converted lower case string is: "+s.toLowerCase());
+    }
+}
