fbce6f2af36d487dd0629cdb813cdf589ecbcdb6
Poonam123-qa <poonamg.qa.com>
2024-10-13 19:14:17 +0530

    Finding the file size

---
+++ src/main/java/JavaPractice/FileSize.java
@@ -0,0 +1,10 @@
+package JavaPractice;
+
+import java.io.File;
+
+public class FileSize {
+    public static void main(String[] args) {
+
+        System.out.println("\"C:\\Users\\poona\\Downloads\\purple-flower-10.jpg\": "+new File("purple-flower-10.jpg").length()+" bytes");
+    }
+}
