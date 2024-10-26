08e24e57f598e5a1b2aa5a7ba12b24b4d14b8310
AnkitG-QA <gangurdeankit19@gmail.com>
2024-10-26 20:48:05 +0530

    Find the penultimate word of a sentence

---
+++ src/main/java/JavaPractice/LastWordFind.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class LastWordFind {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String word = sc.nextLine();
+        String[] words = word.trim().split("\\s+"); // Split by whitespace
+
+        if (words.length <2 ){
+            System.out.println("Not enough words");
+        }
+        else{
+            System.out.println(words[words.length-2]);
+        }
+    }
+}
