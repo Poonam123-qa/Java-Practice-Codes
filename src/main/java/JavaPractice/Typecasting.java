098fa71010bb156c984d6303f96983c85f6cad91
Poonam123-qa <poonamg.qa@gmail.com>
2024-11-17 20:25:30 +0530

    Explicit Type casting

---
+++ src/main/java/JavaPractice/Typecasting.java
@@ -0,0 +1,14 @@
+package JavaPractice;
+
+public class Typecasting {
+    public static void main(String[] args) {
+
+        char a='c';
+        byte b = (byte) a;
+
+        double d=10.5;
+        byte p=(byte)d;
+        System.out.println("Print the explicit type casting value..."+b);
+        System.out.println("Print the explicit type casting value..."+p);
+    }
+    }
