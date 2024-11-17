e3a404e400d14f613a0b54d412b3e54b824d673d
Poonam123-qa <poonamg.qa@gmail.com>
2024-11-17 20:53:39 +0530

    If Else conditions

---
+++ src/main/java/Conditions/IfElse.java
@@ -0,0 +1,18 @@
+package Conditions;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int age= sc.nextInt();
+
+        if (age >=18) {
+            System.out.println("Eligible for voting....");
+        }
+        else {
+            System.out.println("Not Eligible for voting....");
+
+        }
+    }
+}
