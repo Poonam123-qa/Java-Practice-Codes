85b2fefeb63025fde3a6c823b2884ea514f714ab
Poonam123-qa <poonamg.qa@gmail.com>
2024-11-24 22:31:37 +0530

    Static and non-static method execution

---
+++ src/main/java/Methods/Staticmethod.java
@@ -0,0 +1,33 @@
+package Methods;
+
+public class Staticmethod {
+
+    public Staticmethod() {
+        System.out.println("Constructor executed.");   //Constructor will be called at time of object creations
+    }
+    static {
+        System.out.println("Static block 1");         // Static anonymous block always executes first before the main method execution and only once it will be executed
+    }
+    {
+        System.out.println("Annonymus block");         //Anonymous block always execute at last after all the execution
+    }
+
+    static void test(){
+        System.out.println("Non static method");        //if the method is non-static then need to create object for calling the method
+
+    }
+    static void text(){
+        System.out.println("Static block 2");           //if the method is static then no need to create object for calling method
+    }
+
+    public static void main(String args[]){
+        System.out.println("Main Method");
+        Staticmethod st=new Staticmethod();
+        st.test();
+        text();
+    }
+
+    static {
+        System.out.println("Static block 3");
+    }
+}
