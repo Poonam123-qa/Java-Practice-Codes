a681f2437ad9cd79ed7d44250503e8082f321c15
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 18:31:02 +0530

    Finding index of value using array

---
+++ src/main/java/Array/Indexfind.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Indexfind {
+    public static void main(String args[]) {
+        int a[]={10, 20, 40, 50, 60};
+
+        System.out.println("The index value is "+a[0]);
+        System.out.println("The index value is "+a[1]);
+        System.out.println("The index value is "+a[2]);
+        System.out.println("The index value is "+a[3]);
+        System.out.println("The index value is "+a[4]);
+
+    }
+
+    }
