c64bc9c26b6cff6e16a5bbf9ebb05a9b17d46c7d
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 19:11:09 +0530

    Finding max length using array

--- src/main/java/Array/LengthOfArray.java
+++ src/main/java/Array/LengthOfArray.java
@@ -1,7 +1,9 @@
 package Array;
 
+import java.lang.String;
+
 public class LengthOfArray {
-    public static void main(String args[]) {
+    public static void main(String args[]){
 
         int a[]={10, 20, 40, 50, 60};
         System.out.println("The length of the array is "+a.length);
