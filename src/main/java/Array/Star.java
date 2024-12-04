d1620b26f2f24a9a4e59895b1eaaf24b93fc03ce
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 19:13:39 +0530

    Printing star using array

--- src/main/java/Array/Star.java
+++ src/main/java/Array/Star.java
@@ -1,7 +1,9 @@
 package Array;
 
+import java.lang.String;
+
 public class Star {
-    public static void main(String args[]) {
+    public static void main(String args[]){
        int n=5;
        int a[][]=new int[n][n];
        for(int i=0;i<a.length;i++){
