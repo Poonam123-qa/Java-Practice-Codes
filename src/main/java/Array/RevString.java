c7fc47991b8fcdcca87c5f2a79cb0f4cb55437b2
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 19:17:19 +0530

    Reverse string using string builder

---
+++ src/main/java/Array/RevString.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class RevString {
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("Poonam");
+        System.out.println(sb.reverse().toString());
+    }
+}
