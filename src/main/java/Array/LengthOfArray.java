871b820b2f41b05a1a98c6cbd1077294fb2671a6
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 18:12:50 +0530

    Length of the arrays

---
+++ src/main/java/Array/LengthOfArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class LengthOfArray {
+    public static void main(String args[]) {
+
+        int a[]={10, 20, 40, 50, 60};
+        System.out.println("The length of the array is "+a.length);
+    }
+
+    }
