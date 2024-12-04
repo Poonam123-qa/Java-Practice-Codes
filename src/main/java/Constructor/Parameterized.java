897b5eed1c96e2a365f0d9cdbfbbc3b860ac47c6
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-04 17:48:28 +0530

    Parameterized constructor

--- src/main/java/Constructor/Parameterized.java
+++ src/main/java/Constructor/Parameterized.java
@@ -3,9 +3,9 @@ package Constructor;
 public class Parameterized {
 
     String name, color;
-    Parameterized(String Name, String color){
+    Parameterized(String Name, String color){               //passing the value when the obj is created and taking the variable from the class
 
-        this.name=Name;
+        this.name=Name;                                     // this keyword indicates that the variable taking form the current class
         this.color=color;
     }
     public static void main(String args[]) {
