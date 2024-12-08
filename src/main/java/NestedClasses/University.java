047c8ad8963ae94bf322ce3a434564df184f6c14
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-08 22:41:24 +0530

    Static Nested Class

--- src/main/java/NestedClasses/University.java
+++ src/main/java/NestedClasses/University.java
@@ -1,4 +1,7 @@
 package NestedClasses;
+//Write a Java program to create an outer class called University with a static nested class Department.
+// The Department class should have a method "displayInfo()" that prints the department name and the number of faculty members.
+// Instantiate the Department class from the main method and call the "displayInfo()" method.
 
 public class University {
 
