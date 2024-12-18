ff4550108c837ca95c5be9af4f11e65bd6cc7b4b
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-18 18:57:12 +0530

    Has A relation Aggregation

--- src/main/java/HasAAggregation/Car.java
+++ src/main/java/HasAAggregation/Car.java
@@ -3,8 +3,8 @@ package HasAAggregation;
 public class Car {
 
     void startCar(){
-        new Engine().stratEngine();              // If you don't want to use inheritance then use has a or is a relation
-        new Tyers().rolling();
+        new Engine().stratEngine();              // If you don't want to use inheritance then use has a
+        new Tyers().rolling();                   //Is a relation means the inheritance
     }
 
     public static void main(String args[]){
