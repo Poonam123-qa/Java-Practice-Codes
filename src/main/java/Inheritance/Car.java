850f9d42574cdb29b2c2392f366a0eadd0ed092c
Poonam123-qa <poonamg.qa@gmail.com>
2024-12-18 12:24:25 +0530

    Single Inheritance including vehicle and car class

--- src/main/java/Inheritance/Car.java
+++ src/main/java/Inheritance/Car.java
@@ -2,7 +2,7 @@ package Inheritance;
 //Write a Java program to create a class called  Vehicle with a method called drive().
 // Create a subclass called Car that overrides the drive() method to print "Repairing a car".
 
-public class Car {
+public class Car extends Vehicle{
 
     public void drive() {
         // Print "Repairing a Car" to the console
