94955ed94ec38f5fd4a4469cfe1a090e56c8d998
Poonam123-qa <poonamg.qa@gmail.com>
2025-06-28 22:37:03 +0530

    Inheritance Family example

---
+++ src/main/java/Inheritance/Baby.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Baby extends Son{
+
+    public static void main(String[] args) {
+        Baby b1=new Baby();
+        b1.Fhome();
+        b1.Shome();
+
+    }
+}
---
+++ src/main/java/Inheritance/Father.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class Father {
+
+    void Fhome(){
+        System.out.println("This is the fathers home...");
+    }
+}
